1f4fb9094b1b12ca06cceb93594389e8ec1c9c82
ThatBaldDude4 <taylorv0004@gmail.com>
2026-09-04 16:52:50 -0500

    wire route for new item form

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,13 +2,19 @@ import { Router } from "express";
 import { 
     getProductController,
     getProductFormController,
-    postProductController 
+    postProductController,
+    getNewProductController, 
+    postNewProduct
 } from "../controllers/productsController.js";
 const productsRouter = Router();
 
 // need to preserve users selected category
 // so that when client is redirected to home 
 // page the products are the same
+
+productsRouter.get("/new", getNewProductController);
+
+productsRouter.post("/new", postNewProduct)
 
 productsRouter.get("/:id", getProductController);
 

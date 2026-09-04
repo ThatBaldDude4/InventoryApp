5ea9c2fc0873eefe08f3d7945e3e0a7c93d8439a
ThatBaldDude4 <taylorv0004@gmail.com>
2026-09-04 17:02:25 -0500

    add normalizeItem for preparing item data for DB

--- db/normalizeItemForDb.js
+++ db/normalizeItemForDb.js
@@ -1,5 +1,12 @@
-import { getAllBrands, getAllCategories, getAllDiscTypes } from "./queries.js";
 import { normalizeRow } from "./parseCsv.js";
+import { 
+    getAllBrands, 
+    getAllCategories, 
+    insertBrand, 
+    insertCategory,
+    getAllDiscTypes,
+    insertDiscType
+} from "./queries.js";
 
 async function dbMaps() {
     const brands = await getAllBrands();
@@ -30,45 +37,49 @@ async function dbMaps() {
 
 // function is very similar to seedDatabase
 // might refactor functions to avoid duplication
-async function normalizeItem(item) {
-    const {brandsMap, categoriesMap, disc_typeMap} = await dbMaps(item);
+async function normalizeItems(items) {
+    const {brandsMap, categoriesMap, disc_typeMap} = await dbMaps();
 
-    let nRow = normalizeRow(item);
-    if (brandsMap.has(nRow.brand)) {
-        nRow.brand = brandsMap.get(nRow.brand);
-    }else {
-        // insert brand into table
-        // set brand to foreign key
-        const brandId = await insertBrand(nRow.brand);
-        brandsMap.set(nRow.brand, brandId);
-        nRow.brand = brandId;
-    }
-    if (categoriesMap.has(nRow.category)) {
-        nRow.category = categoriesMap.get(nRow.category);
-    }else {
-        // insert brand into table
-        // set category as foreign key
-        const categoryId = await insertCategory(nRow.category);
-        categoriesMap.set(nRow.category, categoryId);
-        nRow.category = categoryId;
-    }
-    // disc_type is optional so it needs to cover undefined/null
-    if (disc_typeMap.has(nRow.disc_type)) {
-        nRow.disc_type = disc_typeMap.get(nRow.disc_type);
-    }else if (nRow.disc_type === "" || !nRow.disc_type) {
-        nRow.disc_type = null;
-    }else {
-        const disc_typeId = await insertDiscType(nRow.disc_type);
-        disc_typeMap.set(nRow.disc_type, disc_typeId);
-        nRow.disc_type = disc_typeId;
-    }
-    nRow.description = nRow.description === "" ? null : nRow.description;
-    nRow.price = nRow.price === "" ? null : Number(nRow.price);
-    nRow.quantity = nRow.quantity === "" ? null : Number(nRow.quantity);
+    let result = [];
+    for (const item of items) {
+        let nRow = normalizeRow(item);
+        if (brandsMap.has(nRow.brand)) {
+            nRow.brand = brandsMap.get(nRow.brand);
+        }else {
+            // insert brand into table
+            // set brand to foreign key
+            const brandId = await insertBrand(nRow.brand);
+            brandsMap.set(nRow.brand, brandId);
+            nRow.brand = brandId;
+        }
+        if (categoriesMap.has(nRow.category)) {
+            nRow.category = categoriesMap.get(nRow.category);
+        }else {
+            // insert brand into table
+            // set category as foreign key
+            const categoryId = await insertCategory(nRow.category);
+            categoriesMap.set(nRow.category, categoryId);
+            nRow.category = categoryId;
+        }
+        // disc_type is optional so it needs to cover undefined/null
+        if (disc_typeMap.has(nRow.disc_type)) {
+            nRow.disc_type = disc_typeMap.get(nRow.disc_type);
+        }else if (nRow.disc_type === "" || !nRow.disc_type) {
+            nRow.disc_type = null;
+        }else {
+            const disc_typeId = await insertDiscType(nRow.disc_type);
+            disc_typeMap.set(nRow.disc_type, disc_typeId);
+            nRow.disc_type = disc_typeId;
+        }
+        nRow.description = nRow.description === "" ? null : nRow.description;
+        nRow.price = nRow.price === "" ? null : Number(nRow.price);
+        nRow.quantity = nRow.quantity === "" ? null : Number(nRow.quantity);
 
-    return nRow;
+        result.push(nRow);
+    };
+    return result
 }
 
 export {
-    normalizeItem
+    normalizeItems
 }
